8a38009c2fdefa1ed3652927fcc9d97a01211cf2
codenisbe <codenisbe@protonmail.com>
2025-01-24 09:39:15 +0100

    local run fixes

--- src/main/java/rcr/RcrApplication.java
+++ src/main/java/rcr/RcrApplication.java
@@ -2,10 +2,7 @@ package rcr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationListener;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.event.ContextClosedEvent;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -16,14 +13,6 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 public class RcrApplication {
 
     public static void main(String[] args) {
-        SpringApplication application = new SpringApplication(RcrApplication.class);
-        application.setRegisterShutdownHook(true);
-        ConfigurableApplicationContext context = application.run(args);
-        context.addApplicationListener((ApplicationListener<ContextClosedEvent>) event -> {
-            if ((event.getSource() instanceof org.springframework.context.annotation.AnnotationConfigApplicationContext)) {
-                return;
-            }
-        });
-
+        SpringApplication.run(RcrApplication.class);
     }
 }
